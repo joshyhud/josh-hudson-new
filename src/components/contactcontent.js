6420656c0ba9747e58ad4595c334f3de554df0af
joshyhud <joshhudsondev@gmail.com>
2021-02-21 09:34:54 +0000

    netlify recpatcha

--- src/components/contactcontent.js
+++ src/components/contactcontent.js
@@ -9,7 +9,7 @@ export default function ContactContent(){
 
 			<p>If you would like to get in touch to discuss a potential project or just for a chat, reach out on the form below or contact me on social.</p>
 
-			<form method="post" action="https://getform.io/f/6e8428d4-bb9c-4066-ab2e-b2dd212930fa">
+			<form data-netlify-recaptcha="true" method="post" action="https://getform.io/f/6e8428d4-bb9c-4066-ab2e-b2dd212930fa">
 			  <label>
 			    Name
 			    <input type="text" name="name" id="name" />
@@ -23,7 +23,7 @@ export default function ContactContent(){
 			    <textarea name="message" id="message" rows="5" />
 			  </label>
 			  <button type="submit">Send</button>
-			  <div class="g-recaptcha" data-sitekey="6LeqbwMaAAAAADKhtz3ISBtzSLPPcANedLIjea_2"></div>
+			  <div data-netlify-recaptcha="true"></div>
 			</form>
 
 		</div>
