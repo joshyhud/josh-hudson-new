999b43971e44df93ea9fd40d154007e21b9201d6
joshyhud <joshhudsondev@gmail.com>
2021-02-21 09:45:19 +0000

    amend to contact form

--- src/components/contactcontent.js
+++ src/components/contactcontent.js
@@ -9,21 +9,26 @@ export default function ContactContent(){
 
 			<p>If you would like to get in touch to discuss a potential project or just for a chat, reach out on the form below or contact me on social.</p>
 
-			<form name="contact" data-netlify-recaptcha="true" data-netlify="true" method="POST">
-			  <label>
-			    Name
-			    <input type="text" name="name" id="name" />
-			  </label>
-			  <label>
-			    Email
-			    <input type="email" name="email" id="email" />
-			  </label>
-			  <label>
-			    Message
-			    <textarea name="message" id="message" rows="5" />
-			  </label>
-			  <button type="submit">Send</button>
-			  <div data-netlify-recaptcha="true"></div>
+			<form name="contact" method="POST" data-netlify="true">
+			  <p>
+			    <label>Your Name: <input type="text" name="name" /></label>   
+			  </p>
+			  <p>
+			    <label>Your Email: <input type="email" name="email" /></label>
+			  </p>
+			  <p>
+			    <label>Your Role: <select name="role[]" multiple>
+			      <option value="leader">Leader</option>
+			      <option value="follower">Follower</option>
+			    </select></label>
+			  </p>
+			  <p>
+			    <label>Message: <textarea name="message"></textarea></label>
+			  </p>
+			    <div data-netlify-recaptcha="true"></div>
+			  <p>
+			    <button type="submit">Send</button>
+			  </p>
 			</form>
 
 		</div>
