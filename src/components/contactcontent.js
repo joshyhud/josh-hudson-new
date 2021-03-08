8237e94a1b9c4fc34a3c20b1cdd9749adeb427c8
joshyhud <joshhudsondev@gmail.com>
2021-03-08 09:31:17 +0000

    amended form

--- src/components/contactcontent.js
+++ src/components/contactcontent.js
@@ -9,22 +9,14 @@ export default function ContactContent(){
 
 			<p>If you would like to get in touch to discuss a potential project or just for a chat, reach out on the form below or contact me on social.</p>
 
-			<form method="post" action="https://getform.io/f/6e8428d4-bb9c-4066-ab2e-b2dd212930fa">
-			  <label>
-			    Name
-			    <input type="text" name="name" id="name" />
-			  </label>
-			  <label>
-			    Email
-			    <input type="email" name="email" id="email" />
-			  </label>
-			  <label>
-			    Message
-			    <textarea name="message" id="message" rows="5" />
-			  </label>
-			  <button type="submit">Send</button>
-			  <div class="g-recaptcha" data-sitekey="6LeqbwMaAAAAADKhtz3ISBtzSLPPcANedLIjea_2"></div>
-			</form>
+				<form action="https://getform.io/f/6e8428d4-bb9c-4066-ab2e-b2dd212930fa" method="POST">
+
+				  <input type="text" name="name">
+				  <input type="email" name="email">
+				  <input type="text" name="message">
+				  <button type="submit">Send</button>
+
+				</form>
 
 		</div>
 
--- src/posts/seotest.md
+++
@@ -1,8 +0,0 @@
----
-title: Seo test
-date: 2021-02-27
-featured: images/seo.jpg
-description: SEO test description
----
-
-Hello World!
