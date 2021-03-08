e267f50e8d525128772d34e04c8b001efbc9800a
joshyhud <joshhudsondev@gmail.com>
2021-03-08 09:39:33 +0000

    amended form

--- src/components/contactcontent.js
+++ src/components/contactcontent.js
@@ -9,14 +9,16 @@ export default function ContactContent(){
 
 			<p>If you would like to get in touch to discuss a potential project or just for a chat, reach out on the form below or contact me on social.</p>
 
-				<form action="https://getform.io/f/6e8428d4-bb9c-4066-ab2e-b2dd212930fa" method="POST">
+			<form method="post" action="https://getform.io/f/6e8428d4-bb9c-4066-ab2e-b2dd212930fa">
+			  <label>
+			    Name
+			    <input type="text" name="name" id="name" />
+			    <textarea name="message" id="message" rows="5" />
+			  </label>
+			  <button type="submit">Send</button>
+			  <div class="g-recaptcha" data-sitekey="6LeqbwMaAAAAADKhtz3ISBtzSLPPcANedLIjea_2"></div>
+			</form>
 
-				  <input type="text" name="name">
-				  <input type="email" name="email">
-				  <input type="text" name="message">
-				  <button type="submit">Send</button>
-
-				</form>
 
 		</div>
 
