4e572a49a9caebfd9bc6eccbc87e59300e80ab5e
joshyhud <joshhudsondev@gmail.com>
2021-03-08 09:36:27 +0000

    amended form

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,27 +3,18 @@ import { graphql } from "gatsby"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import Container from "../components/container"
-import postStyles from "../styles/post.module.css"
-import SEO from "../components/seo"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
-  const image = post.frontmatter.image
-
   return (
     <Container>
-    	  <Header />
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-          image={image}
-        />
-      <div className={postStyles.article}>
+        <Header />
+      <div>
         <h1>{post.frontmatter.title}</h1>
         <h4>{post.frontmatter.date}</h4>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
-      	  <Footer />
+          <Footer />
     </Container>
   )
 }
@@ -34,8 +25,6 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "DD MMMM, YYYY")
-        description
-        image: featured
       }
     }
   }
