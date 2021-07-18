eba66c099e48381a0396f5689c120b050cbf7499
joshyhud <joshhudsondev@gmail.com>
2021-07-18 12:46:17 +0100

    contact form amendments

--- src/components/contactcontent.js
+++ src/components/contactcontent.js
@@ -9,19 +9,19 @@ export default function ContactContent(){
 
 			<p>If you would like to get in touch to discuss a potential project or just for a chat, reach out on the form below or contact me on social.</p>
 
-			<form name="Contacts" method="POST" data-netlify="true">
-			  <label>
-			    Name
-			    <input type="text" name="name" id="name" />
-			  </label>
-			  <label>
-			    Email
-			    <input type="email" name="email" id="email" />
-			  </label>
-			  <label>
-			    Message
-			    <textarea name="message" id="message" rows="5" />
-			  </label>
+			<form name="Contacts" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+			  <p>
+			    <label>Name: <input type="text" name="name"></input></label>
+			  </p>
+			  <p>
+			    <label>Email: <input type="text" name="email" /></label>
+			  </p>
+			  <p>
+			    <label>Message: <textarea name="message"></textarea></label>
+			  </p>
+			    <p class="hidden">
+				    <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+				</p>
 			  <button type="submit">Send</button>
 			</form>
 
