9bed42161b2a920ce679861cc94599ace920e1ff
joshyhud <joshhudsondev@gmail.com>
2021-07-18 11:15:45 +0100

    seo additions

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,9 +7,20 @@ import SEO from "../components/seo"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
+  const title = data.site.siteMetadata.title
+    const image = post.frontmatter.image
+      ? post.frontmatter.image.childImageSharp.resize
+      : null
+
   return (
     <Container>
-        <Header />
+        <SEO
+          title={post.frontmatter.title}
+          description={post.frontmatter.description || post.excerpt}
+          image={image}
+          pathname={this.props.location.pathname}
+        />
+      <Header />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <h4>{post.frontmatter.date}</h4>
@@ -21,11 +32,27 @@ export default function BlogPost({ data }) {
 }
 export const query = graphql`
   query($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
         title
+        descriptionfetured.
         date(formatString: "DD MMMM, YYYY")
+        image: featured {
+          childImageSharp {
+            resize(width: 1200) {
+              src
+              height
+              width
+            }
+          }
+        }
       }
     }
   }
